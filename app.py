5a2ad7a0da28f17aa4da4b5f9be9fb4f8c02249e
adityapanda045 <135442292+adityapanda045@users.noreply.github.com>
2026-04-22 20:27:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def get_gemini_reponse(input_text, system_prompt):
     # Using 'gemini-1.5-flash' which is the 2026 standard for speed and reliability
-    model = genai.GenerativeModel('gemini-pro')
+    genai.GenerativeModel('models/gemini-1.5-flash')
     # Combining instructions and data into a single clear 'Content' block
     response = model.generate_content([system_prompt, input_text])
     return response.text
